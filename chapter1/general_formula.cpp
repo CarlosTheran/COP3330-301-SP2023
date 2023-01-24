fc1ebed671f952fea89a24228b919b6fedac469f
CarlosTheran <carlos.theran@upr.edu>
2023-01-24 13:30:50 -0500

    finished

--- chapter1/general_formula.cpp
+++ chapter1/general_formula.cpp
@@ -8,6 +8,7 @@
 #include <math.h> // library is use for math functions
 #include <iomanip> //library is use for manipulator (setw and setprecision)
 #include <string>
+#include <stdio.h>
 
 using namespace std;
 
@@ -31,11 +32,39 @@ void general_formula(double a, double b, double c)
    }
 }
 
+
+void evaluation(double a, double b, double c, double x_value[], double y_value[], int leng)
+{
+   for(int i=0;i<leng;i++)
+   {
+      y_value[i] = a*pow(x_value[i],2)+b*x_value[i]+c;
+      cout<<"For x="<<x_value[i]<<"; y="<<y_value[i]<<endl;
+   }
+}
+
+
+double evaluate(double a, double b, double c, double x)
+{
+   return a*pow(x,2)+b*x+c;
+}
+
+void vertex(double a, double b, double c)
+{
+   double vertex1 = 0.0;
+   double vertex2 = 0.0;
+
+   vertex1 = -b/(2*a);
+   vertex2 = evaluate(a,b,c,vertex1);
+
+   printf("\n The vertex are:(%.4f, %.4f)",vertex1,vertex2);
+}
+
 int main()
 {
    double a =0.0, b=0.0, c=0.0;
-
-
+   double x_values[] = {2, 3, 4, 6, 7, -4};
+   double y_values[6] = {};
+   int leng = 6;
    string answer = "yes";
    string answer2 = "yes";
 
@@ -45,6 +74,8 @@ int main()
       cin>> a >> b >> c;
 
       general_formula( a,  b,  c);
+      evaluation( a, b, c, x_values, y_values, leng);
+      vertex(a,b,c);
       cout<<"\nDo you want to keep using the APP? ";
       cin >> ws;
       getline(cin,answer);
